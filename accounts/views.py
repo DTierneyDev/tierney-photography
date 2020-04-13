c9a0a382d998e645528b9e416982711ab40a5bed
Daniel Tierney <immortals11@hotmail.co.uk>
2020-04-13 10:42:23 +0000

    Login and registration now fully working. Profile page also setup.

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
-from accounts.forms import UserLoginForm
+from django.contrib.auth.models import User
+from accounts.forms import UserLoginForm, UserRegistrationForm
 
 
 def index(request):
-    """Return the login.html file"""
+    """Return the index.html file"""
     return render(request, 'index.html')
 
 
@@ -39,3 +40,36 @@ def logout(request):
     auth.logout(request)
     messages.success(request, "You have successfully been logged out!")
     return redirect(reverse('index'))
+
+
+def registration(request):
+    """Render the registration.html page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+
+    if request.method == "POST":
+        registration_form = UserRegistrationForm(request.POST)
+
+        if registration_form.is_valid():
+            registration_form.save()
+
+            user = auth.authenticate(username=request.POST['username'],
+                                    password=request.POST['password1'])
+
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully registered!")
+                return(redirect(reverse('index')))
+            else:
+                messages.error(request, "Unable to register your account at this time")
+    else:
+        registration_form = UserRegistrationForm()
+
+    return render(request, 'registration.html', {"registration_form": registration_form})
+
+
+@login_required
+def user_profile(request):
+    """Render the user's profile page"""
+    user = User.objects.get(email=request.user.email)
+    return render(request, 'profile.html', {"profile": user})
